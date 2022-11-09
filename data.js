2d444bad3896af27b00c836ffd84d9c779a0c607
Manoj P <manu8892405514@gmail.com>
2022-11-09 00:57:40 -0800

    test 3

--- data.js
+++ data.js
@@ -1,122 +1,122 @@
 export default [
-    {
-    "id": 1,
-    "name": "Test hhhh",
-    "image": "http://dummyimage.com/171x100.png/ff4444/ffffff",
-    "ingredients": "item 5 item 1 item 2  item 3",
-    "instruction": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
-    "rate": 3
-  },
   {
-    "id": 2,
-    "name": "Wolf's Opuntia",
-    "image": "http://dummyimage.com/235x100.png/5fa2dd/ffffff",
-    "ingredients": "item 5item 1 item",
-    "instruction": "Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.",
-    "rate": 5
-  },
-  {
-    "id": 3,
-    "name": "Brown Stringybark",
-    "image": "http://dummyimage.com/192x100.png/ff4444/ffffff",
-    "ingredients": "item 1 item 2  item 3  item 4",
-    "instruction": "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
-    "rate": 4
-  },
-  {
-    "id": 4,
-    "name": "Holmes' Hawthorn",
-    "image": "http://dummyimage.com/117x100.png/5fa2dd/ffffff",
-    "ingredients": "item 1 item 2  item 3  item 4",
-    "instruction": "Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.",
-    "rate": 2
-  },
-  {
-    "id": 5,
-    "name": "Chapman's Crownbeard",
-    "image": "http://dummyimage.com/154x100.png/dddddd/000000",
-    "ingredients": "item 1 item 2  item 3  item 4",
-    "instruction": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.",
-    "rate": 5
-  },
-  {
-    "id": 6,
-    "name": "Gredin's Whitlowgrass",
-    "image": "http://dummyimage.com/188x100.png/dddddd/000000",
-    "ingredients": "item 4  item 5 item 1 item 2",
-    "instruction": "Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus.",
-    "rate": 5
-  },
-  {
-    "id": 7,
-    "name": "Neckera Moss",
-    "image": "http://dummyimage.com/139x100.png/ff4444/ffffff",
-    "ingredients": "item 1 item 2  item 3",
-    "instruction": "Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
-    "rate": 2
-  },
-  {
-    "id": 8,
-    "name": "Scarlet Monkeyflower",
-    "image": "http://dummyimage.com/177x100.png/ff4444/ffffff",
-    "ingredients": "item 3  item 4  item 5 item 1 item 2  item 3  item 4  item 5",
-    "instruction": "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
-    "rate": 4
-  },
-  {
-    "id": 9,
-    "name": "Pseudoparmelia",
-    "image": "http://dummyimage.com/159x100.png/cc0000/ffffff",
-    "ingredients": "item 5 item 1 item 2  item 3",
-    "instruction": "In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
-    "rate": 2
-  },
-  {
-    "id": 10,
-    "name": "Laurel Espada",
-    "image": "http://dummyimage.com/162x100.png/dddddd/000000",
-    "ingredients": "item 4  item 5 item 1 item 2  item 3  item 4",
-    "instruction": "Phasellus in felis. Donec semper sapien a libero. Nam dui.",
-    "rate": 5
-  },
-  {
-    "id": 11,
-    "name": "Tylophoron Lichen",
-    "image": "http://dummyimage.com/231x100.png/ff4444/ffffff",
-    "ingredients": "item 1 item 2  item 3  item 4",
-    "instruction": "Fusce consequat. Nulla nisl. Nunc nisl.",
-    "rate": 5
-  },
-  {
-    "id": 12,
-    "name": "Cascade Aster",
-    "image": "http://dummyimage.com/172x100.png/dddddd/000000",
-    "ingredients": "item 1 item 2  item 3",
-    "instruction": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
-    "rate": 4
-  },
-  {
-    "id": 13,
-    "name": "Lecania Lichen",
-    "image": "http://dummyimage.com/183x100.png/5fa2dd/ffffff",
-    "ingredients": "item 4  item 5 item 1 item 2  item 3",
-    "instruction": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
-    "rate": 3
-  },
-  {
-    "id": 14,
-    "name": "Rockcastle Aster",
-    "image": "http://dummyimage.com/201x100.png/5fa2dd/ffffff",
-    "ingredients": "item 4  item 5 item 1 item 2  item 3  item 4",
-    "instruction": "Fusce consequat. Nulla nisl. Nunc nisl.",
-    "rate": 3
-  },
-  {
-    "id": 15,
-    "name": "Eschweiler's Pyxine Lichen",
-    "image": "http://dummyimage.com/133x100.png/ff4444/ffffff",
-    "ingredients": "item 5item 1 item",
-    "instruction": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
-    "rate": 3
-  }
+  "id": 1,
+  "name": "Payyoli Chicken Fry",
+  "image": "https://c.ndtvimg.com/2022-06/1tg3ubu8_payyoli-chicken-fry_625x300_22_June_22.jpeg",
+  "ingredients": "500 Gram Chicken, cleaned and cut into 1 inch cubes1 tsp Garlic, minced1 tsp Ginger, minced2 tsp Kashmiri Red Chilli Powder1/2 tsp Turmeric Powder (Haldi)2 tbsp Vinegar1 Green Chilli, slit lengthwise1 tbsp Meat Masala3 tbsp Fresh Coconut, gratedto taste Salt2 sprig Curry Leaves3 tbsp Coconut Oil100 ml Water",
+  "instruction": "1.Marinate the chicken in minced ginger and garlic, vinegar, turmeric powder, salt, meat masala powder and Kashmiri red chilli powder. Keep this for an hour.2.Combine grated coconut and Kashmiri red chilli powder.3.Heat coconut oil in a pan and fry the marinated chicken along with slit green chillies.4.Add the red chilli-coconut mix and curry leaves to the semi-cooked chicken and fry it for a while.5.Pour water and let the chicken cook till the chicken completely absorbs the water.6.The Payyoli chicken fry is ready!",
+  "rate": 3
+},
+{
+  "id": 2,
+  "name": "Hyderabadi Dum Chai",
+  "image": "https://c.ndtvimg.com/2022-06/a0vcbi1_chai_625x300_16_June_22.jpg",
+  "ingredients": "2 Cups Water1.5 tbsp Tea leaves1 tbsp Condensed Milk1/2 cup Milk",
+  "instruction": "1.Take two cups of water in a pan and bring it to a boil. Now add tea leaves and cardamom to this.2.Now in another pan, take some milk and simmer it till it thickens.3.Add the thickened milk in with the boiling water.4.Now add some condensed milk also. Let this boil.5.Once done, pour in your cups and relish!",
+  "rate": 5
+},
+{
+  "id": 3,
+  "name": "Palak Vada ",
+  "image": "https://c.ndtvimg.com/2020-12/25bbsmjo_methi-fritter_625x300_02_December_20.jpg",
+  "ingredients": "1 and a half Chana Dal (Soaked)1 cup Spinach (Chopped)2-3 Green Chillies (Chopped)1 tbsp Ginger Garlic Paste1 tsp Red Chilli Powder1 tsp Dry Fenugreek Leaves1/2 tsp Dry Mango PowderSalt1 tsp Cumin Seeds",
+  "instruction": "1.Take a grinder, add soaked (soaked at least for 4 hours) chana dal and grind it until a thick paste.2.Then take out the blended paste in a bowl, add fenugreek leaves, cumin seeds, green chillies, ginger garlic paste and other dry ingredients.3.Mix everything well. Add a little amount of water (if required).4.Take small portion out of the batter one by one. Heat oil in a kadhai and deep fry them.5.Pair these winter special vadas with ketchup, mint or tamarind chutney.",
+  "rate": 4
+},
+{
+  "id": 4,
+  "name": "Paneer Masala Fry",
+  "image": "https://c.ndtvimg.com/2021-12/gh4paaqg_paneer-masala-fry_625x300_08_December_21.png",
+  "ingredients": "7-8 Paneer Cubes1/2 cup Curd1/2 tsp Red Chilli Powder1/2 tsp Black Pepper1/2 tsp Garam Masala1/2 tsp Coriander Powder1/2 tsp Turmeric Powder1 tbsp Ginger-Garlic Paste5-6 Curry Leaves1 Dry Red Chilli",
+  "instruction": "1.Cut the paneer in small cubes.2.Now in a bowl add red chilli powder, pepper, garam masalas, turmeric powder, coriander powder, ginger-garlic paste and mix.3.Now add thick curd and combine it with the masalas. Finally add some lemon drops.4.Throw in the paneer cubes and make sure that all the pieces are coated well. Let it rest for 15 minutes.5.In a pan, heat some oil, add curry leaves, dry red chilli and let it splutter.6.Put in the paneer pieces and cook from both the sides.7.When it turns brown and crispy take it out and serve!",
+  "rate": 4
+},
+{
+  "id": 5,
+  "name": "Poha Chaat ",
+  "image": "https://c.ndtvimg.com/2021-12/0vvg3qcg_poha_625x300_02_December_21.png",
+  "ingredients": "1 Cup Poha1/2 cup Peanuts2 tbsp Urad Dal6-7 Curry Leaves1 Chopped Onion1 Chopped Tomato1 Chopped Cucumber1/2 tsp Red Chilli Powder1/2 tsp Pepper1/2 tsp Turmeric PowderSalt as per taste",
+  "instruction": "1.In a pan dry roast poha and take it out. Then in the same pan roast peanuts, curry leaves, urad dal, cashews and almonds.2.Once done mix the poha and other rasted ingredients.3.Now throw in chopped onions, cucumber, tomatoes and mix again.4.Finally add, red chilli powder, pepper, salt as per taste.5.Now add a table spoon of imly chutney.6.Finally garnish it with coriander leaves and serve it in a bowl.",
+  "rate": 5
+},
+{
+  "id": 6,
+  "name": "Chana Dal Chikki",
+  "image": "https://c.ndtvimg.com/2021-11/1jpkl19_chana-dal-chikki-_625x300_30_November_21.jpg",
+  "ingredients": "1 Cup Split Chana Dal1 cup Jaggery2 tbsp Ghee",
+  "instruction": "1.In a kadhai, dry roast chana dal until it smells done and starts changing color to slightly golden.2.In another pan heat ghee and add jaggery to it, mix until properly melted.3.Add the roasted chana dal to the jaggery and mix until every piece has been coated with jaggery. Take off the heat and quickly transfer to a well-greased flat surface.4.Let this set for 2-4 hours and enjoy.",
+  "rate": 5
+},
+{
+  "id": 7,
+  "name": "Kerala Egg Curry",
+  "image": "https://c.ndtvimg.com/2021-11/8dmk5frg_kerala-egg-curry_625x300_19_November_21.jpg",
+  "ingredients": "4 Boiled Eggs1 Chopped Onion1 tbsp Ginger Garlic Paste2 Tomatoes1/2 tsp Red Chilli Powder1/2 tsp Pepper1/2 tsp Turmeric Powder1/2 tsp Garam MasalaSalt as per taste",
+  "instruction": "1.Add oil in a pan and heat it.2.Now add mustard seeds and onions and ginger garlic paste.3.Cook till onions are soft. Now add tomato puree, and cook it.4.Next, throw in masalas like red chilli powder, pepper, turmeric powder, garam masala and salt. Mix them well.5.Take boiled eggs, slit them from middle and add it in the pan. Let all the flavours combine.6.Add a dash of coconut milk and let it boil.",
+  "rate": 2
+},
+{
+  "id": 8,
+  "name": "Bread Dahi Chaat",
+  "image": "https://c.ndtvimg.com/2021-06/3r3uj3go_rasgulla-chaat_625x300_24_June_21.jpg",
+  "ingredients": "5-6 Bread3 tbsp Yoghurt1 tbsp Green chutney1 tbsp Imly chutney1/4 tsp Red Chilli Powder1/4 tsp Pepper1/2 tsp Chaat MasalaSalt as per taste",
+  "instruction": "1.Take five to six pieces of bread and cut them in small bite size pieces.2.Now you can either choose to bake it or pan fry it till crispy.3.Once it is done let it come till room temperature and then add it to a bowl.4.To this, now add yoghurt, green chutney, imly chutney, boiled potatoes and boiled chana. Mix again.5.Next, add spices like pepper, red chilli powder, chaat masala and salt as per taste and combine again.6.Lastly, add pomegranate and sev or bhujia from the top to relish!",
+  "rate": 4
+},
+{
+  "id": 9,
+  "name": "3-Ingredient Onion Pickle",
+  "image": "https://c.ndtvimg.com/2021-11/absdprv_3ingredient-onion-pickle_625x300_05_November_21.jpg",
+  "ingredients": "2 Red Onions1 cup Warm Water1/4 cup White Vinegar1 tbsp Salt",
+  "instruction": "1.Take two red onions and slice them thinly. Add this to a jar or bowl.2.Now add warm water and make sure the onions submerge in it.3.To this add white vinegar and sea salt. Mix it all properly so that the flavors combine.4.Keep it in the refrigerator till it gets cold and then serve to enjoy!",
+  "rate": 2
+},
+{
+  "id": 10,
+  "name": "Homemade Bread Gulab Jamun",
+  "image": "https://i.ndtvimg.com/i/2017-09/gulab-jamun_650x400_51505468476.jpg",
+  "ingredients": "8 Bread Slices2 tbsp Malai/cream1/3 cup Milk1 tbsp Maida1 cup Ghee/Neutral oil1 cup Sugar2-4 Cardamom Pods",
+  "instruction": "1.Take bread slices and grind them to make fresh breadcrumbs.2.Add malai and milk powder to this and mash lightly.3.Now add milk in small batches and knead a soft dough.4.Make tiny dough balls out of this and keep aside on a plate.5.Heat ghee/neutral oil in a pan and fry the dough balls in batches.6.Lightly shallow fry until the balls are golden brown.7.While you are frying the dough balls, in another pan heat a cup of water.8.Add the sugar and stir until slightly thick, add crushed cardamom pods and Kesar strands.9.Let the sugar syrup come to a rolling boil then take off the heat.10.Now add the fried dough balls to the sugar syrup, serve hot or cold according to your preference.",
+  "rate": 5
+},
+{
+  "id": 11,
+  "name": "Tylophoron Lichen",
+  "image": "http://dummyimage.com/231x100.png/ff4444/ffffff",
+  "ingredients": "item 1 item 2  item 3  item 4",
+  "instruction": "Fusce consequat. Nulla nisl. Nunc nisl.",
+  "rate": 5
+},
+{
+  "id": 12,
+  "name": "Cascade Aster",
+  "image": "http://dummyimage.com/172x100.png/dddddd/000000",
+  "ingredients": "item 1 item 2  item 3",
+  "instruction": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.",
+  "rate": 4
+},
+{
+  "id": 13,
+  "name": "Lecania Lichen",
+  "image": "http://dummyimage.com/183x100.png/5fa2dd/ffffff",
+  "ingredients": "item 4  item 5 item 1 item 2  item 3",
+  "instruction": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+  "rate": 3
+},
+{
+  "id": 14,
+  "name": "Rockcastle Aster",
+  "image": "http://dummyimage.com/201x100.png/5fa2dd/ffffff",
+  "ingredients": "item 4  item 5 item 1 item 2  item 3  item 4",
+  "instruction": "Fusce consequat. Nulla nisl. Nunc nisl.",
+  "rate": 3
+},
+{
+  "id": 15,
+  "name": "Eschweiler's Pyxine Lichen",
+  "image": "http://dummyimage.com/133x100.png/ff4444/ffffff",
+  "ingredients": "item 5item 1 item",
+  "instruction": "Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.",
+  "rate": 3
+}
 ]
