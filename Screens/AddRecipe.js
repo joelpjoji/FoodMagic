52eec75b3c0628a74224f266617a64d51d033d5f
joelpjoji <joelpjoji1222@gmail.com>
2022-11-10 20:02:06 +0530

    fixed rating on recipe page

--- Screens/AddRecipe.js
+++ Screens/AddRecipe.js
@@ -7,7 +7,9 @@ import {
   Text,
   Button,
   View,
+  TextInput
 } from 'react-native';
+import { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 
@@ -15,24 +17,107 @@ import { useNavigation } from '@react-navigation/native';
 const AddRecipeScreen = () => {
   const navigation = useNavigation();
 
+  const[name, setName] = useState([]);
+  const [instruction, setInstruction]=useState([]);
+  const [ingredients, setIngredients] = useState([]);
+
+    
+   
+
   return (
-    <View>
-      <Text>AddRecipe Screen</Text>
-      <Button
-      title="Home "
-      onPress={() =>
-        navigation.navigate('Home', { name: 'Jane' })
-      }
-    />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <View>
+       <Text>AddRecipe Screen</Text>
+
+       <View>
+          <Text style={styles.name}>Add Recipe</Text>
+       </View>
+
+       <View >
+          <TextInput style={styles.innercontainer1}
+           onChangeText={setName} value={name}
+            placeholder='Recipe name' />
+          <TextInput style={styles.innercontainer2}
+           onChangeText={setInstruction} 
+           value={instruction} 
+           placeholder='Instruction' />
+        </View>
+
+        <Text style={styles.ingredname}>Ingredients</Text>
+
+        
+    <Button
+    title="Submit"
+    onPress={() =>
+      navigation.navigate('Home',)
+    }
+  />
+  </View></ScrollView>
+       
+  </SafeAreaView>
+   
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    //flex: 1,
+    backgroundColor: '#fff',
+    //alignItems: 'flex-start',
+    //justifyContent: 'flex-start',
+    marginStart: 20,
+    marginTop: 50,
+    marginRight: 20,
   },
+    header: {
+        fontSize: 25,
+        color: 'blue',
+       
+    },
+    name: {
+      fontSize: 20,
+      color: 'black',
+      marginTop: 20,
+      marginBottom: 10,
+      
+    },
+    innercontainer1:{
+      padding: 10,
+      borderColor: 'black',
+      borderWidth: 1,
+      padding: 5,   
+    },
+    innercontainer2:{
+      padding: 10,
+      borderColor: 'black',
+      borderWidth: 1,
+      marginTop: 10,
+    },
+    button:{
+      marginTop: 200,
+      
+    },
+    ingredname: {
+      position: 'relative',
+      fontSize: 20,
+      color: 'black',
+      marginTop: 20,
+      marginBottom: 10,
+      
+    },
+    ingredbutton: {
+      position: 'absolute',
+    },
+    ingred: {
+      flexDirection: 'row',
+      alignItems: 'flex-end',
+      justifyContent: 'space-between'
+    }
+
+
+    
+    
 });
 
 export default AddRecipeScreen;
--- Screens/Recipe.js
+++ Screens/Recipe.js
@@ -8,48 +8,103 @@ import {
   Button,
   View,
 } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
+import {useNavigation} from '@react-navigation/native';
+import Rating from './ratings';
 
 const RecipeScreen = props => {
   const {route} = props;
   const {params} = route;
   const {item} = params;
-  const {name, ingredients,instruction,rate, image} = item;
+  const {name, ingredients, instruction, rate, image} = item;
 
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <View >
-      <Image source={{uri: item.image}} style={{width: 200, height: 200}} />
+      <ScrollView>
+        <View style={styles.container}>
+          <View>
+            <Image source={{uri: item.image}} style={styles.image} />
 
-        <Text >{name}</Text>
-      </View>
-      <View>
-        <Text >{ingredients}</Text>
-      </View>
-      <View >
-        <Text >{instruction}</Text>
-      </View>
-      <View>
-        <Text >{rate}</Text>
-      </View>
-      <Button
-      title="Home "
-      onPress={() =>
-        navigation.navigate('Home', { name: 'Jane' })
-      }
-    />
-    </View>
+            <Text style={styles.heading}>{name}</Text>
+          </View>
+          <View>
+            <Text style={styles.ingredientsHeading}>Ingredients</Text>
+            <Text style={styles.ingredients}>{ingredients}</Text>
+          </View>
+          <View>
+            <Text style={styles.instructionHeading}>Instruction</Text>
+            <Text style={styles.instruction}>{instruction}</Text>
+          </View>
+          <View>
+            <Text style={styles.ratingHeading}>
+              Rating :
+              <Text>
+                {Array(item.rate)
+                  .fill('')
+                  .map((data, index) => {
+                    return <Text key={index}>⭐️</Text>;
+                  })}
+              </Text>
+            </Text>
+            <View style={styles.rating}></View>
+          </View>
+        </View>
+      </ScrollView>
   );
 };
-
 
 const styles = StyleSheet.create({
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
+  },
+  image: {
+    height: 200,
+    width: '100%',
+    borderRadius: 10,
+    marginTop: 5,
+  },
+
+  heading: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'tomato',
+    textAlign: 'center',
+  },
+  ingredients: {
+    fontSize: 16,
+    padding: 10,
+  },
+  ingredientsHeading: {
+    fontSize: 30,
+    color: 'blue',
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  instructionHeading: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'blue',
+    padding: 10,
+  },
+  instruction: {
+    fontSize: 16,
+    padding: 10,
+  },
+  ratingHeading: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'blue',
+    padding: 10,
+    marginBottom:20,
+  },
+  rating: {
+    fontSize: 16,
+    padding: 10,
+    flexDirection: 'row',
+  },
+  homebutton: {
+    flexDirection: 'row',
   },
 });
 
