8d381a01bf5d1db3c304644f25e14a3f198df5db
Manoj P <manu8892405514@gmail.com>
2022-11-10 04:09:28 -0800

    recipes screen updated and styled

--- Screens/Recipe.js
+++ Screens/Recipe.js
@@ -20,28 +20,41 @@ const RecipeScreen = props => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <View >
-      <Image source={{uri: item.image}} style={{width: 200, height: 200}} />
-
-        <Text >{name}</Text>
-      </View>
-      <View>
-        <Text >{ingredients}</Text>
-      </View>
-      <View >
-        <Text >{instruction}</Text>
-      </View>
-      <View>
-        <Text >{rate}</Text>
-      </View>
-      <Button
+    <SafeAreaView>
+      <ScrollView>
+        <View style={styles.homebutton}>
+          <Button
       title="Home "
       onPress={() =>
         navigation.navigate('Home', { name: 'Jane' })
       }
-    />
+    /></View>
+      
+      <View style={styles.container}>
+     
+      <View >
+      <Image source={{uri: item.image}} style={styles.image} />
+
+        <Text style={styles.heading}>{name}</Text>
+      </View>
+      <View>
+        <Text style={styles.ingredientsHeading}>Ingredients</Text>
+        <Text style={styles.ingredients} >{ingredients}</Text>
+      </View>
+      <View >
+        <Text style={styles.instructionHeading}>Instruction</Text>
+        <Text style={styles.instruction} >{instruction}</Text>
+      </View>
+      <View>
+        <Text style={styles.ratingHeading}>Rating</Text>
+        <Text style={styles.rating}>{rate}</Text>
+      </View>
+     
     </View>
+      </ScrollView>
+
+    </SafeAreaView>
+    
   );
 };
 
@@ -51,6 +64,61 @@ const styles = StyleSheet.create({
     marginTop: 32,
     paddingHorizontal: 24,
   },
+  image:{
+    height:200,
+    width:"100%",
+
+  },
+  heading:{
+    fontSize:30,
+    fontWeight:"bold",
+    color:"tomato",
+    textAlign:"center",
+  },
+  ingredients:{
+    fontSize:16,
+    padding:10
+
+
+  },
+  ingredientsHeading:{
+
+    fontSize:30,
+    color:"blue",
+    fontWeight:"bold",
+    padding:10,
+
+  },
+  instructionHeading:{
+    fontSize:30,
+    fontWeight:"bold",
+    color:"blue",
+    padding:10,
+
+  },
+  instruction:{
+    fontSize:16,
+    padding:10,
+
+
+  },
+  ratingHeading:{
+    fontSize:30,
+    fontWeight:"bold",
+    color:"blue",
+    padding:10,
+
+  },
+  rating:{
+    fontSize:16,
+    padding:10,
+
+  },
+  homebutton:{
+    flexDirection:"row",
+    
+  }
+
 });
 
 export default RecipeScreen;
